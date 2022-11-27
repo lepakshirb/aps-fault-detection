f2e0529d302ee61b0612afe6fd10b283f4eef9ff
lepakshirb <bharat.bharadwaj5@gmail.com>
2022-11-27 15:46:08 +0530

    setup file is ready

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
 from setuptools import find_packages,setup
-from typing import list
+
+from typing import List
 
 REQUIREMENT_FILE_NAME="requirements.txt"
-
-def get_requirements()->list[str]:
+HYPHEN_E_DOT = "-e ."
+def get_requirements()->List[str]:
+    
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        requirement_list =requirement_file.readlines()
-    requirement_list =[requirement_name.replace("\n","") for requirement_name in requirement_list]
-    if HYPEN_E_DOT in requirement_list:
-        requirement_list.remove(HYPEN_E_DOT)
+        requirement_list = requirement_file.readlines()
+    requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
+    
+    if HYPHEN_E_DOT in requirement_list:
+        requirement_list.remove(HYPHEN_E_DOT)
     return requirement_list
+
 
 
 setup(
